1fa97ab5a73a83572b08003f43721980569780e3
Andrey Bogdanov <the-dark-angel@yandex.ru>
2013-03-19 12:26:32 +0600

    Implement TSampleInfo class

--- src/tsampleinfo.cpp
+++ src/tsampleinfo.cpp
@@ -9,6 +9,11 @@
 #include <QObject>
 #include <QDataStream>
 #include <QString>
+#include <QStringList>
+#include <QDateTime>
+#include <QRegExp>
+#include <QVariant>
+#include <QDebug>
 
 /*============================================================================
 ================================ TSampleInfoPrivate ==========================
@@ -18,13 +23,24 @@ class TSampleInfoPrivate : public BBasePrivate
 {
     B_DECLARE_PUBLIC(TSampleInfo)
 public:
+   static QString numberToString(quint64 number, int fixedLength = -1);
+public:
    explicit TSampleInfoPrivate(TSampleInfo *q);
     ~TSampleInfoPrivate();
 public:
     void init();
 public:
     quint64 id;
+    quint64 authorId;
+    QString authorName;
     QString title;
+    TSampleInfo::Type type;
+    QStringList tags;
+    QString comment;
+    QString remark;
+    quint8 rating;
+    QDateTime creationDT;
+    QDateTime modificationDT;
 private:
     Q_DISABLE_COPY(TSampleInfoPrivate)
 };
@@ -32,6 +48,17 @@ private:
 /*============================================================================
 ================================ TSampleInfoPrivate ==========================
 ============================================================================*/
+
+/*============================== Static public methods =====================*/
+
+QString TSampleInfoPrivate::numberToString(quint64 number, int fixedLength)
+{
+    QString s = QString::number(number);
+    int dlen = fixedLength - s.length();
+    if (dlen > 0)
+        s.prepend( QString().fill('0', dlen) );
+    return s;
+}
 
 /*============================== Public constructors =======================*/
 
@@ -51,11 +78,59 @@ TSampleInfoPrivate::~TSampleInfoPrivate()
 void TSampleInfoPrivate::init()
 {
     id = 0;
+    authorId = 0;
+    type = TSampleInfo::Unverified;
+    rating = 0;
+    creationDT.setTimeSpec(Qt::UTC);
+    modificationDT.setTimeSpec(Qt::UTC);
 }
 
 /*============================================================================
 ================================ TSampleInfo =================================
 ============================================================================*/
+
+/*============================== Static public methods =====================*/
+
+QString TSampleInfo::typeToString(Type t, bool singular)
+{
+    if (singular)
+    {
+        switch (t)
+        {
+        case Approved:
+            return tr("Approved", "type (singular)");
+        case Rejected:
+            return tr("Rejected", "type (singular)");
+        case Unverified:
+        default:
+            return tr("Unverified", "type (singular)");
+        }
+    }
+    else
+    {
+        switch (t)
+        {
+        case Approved:
+            return tr("Approved", "type (plural)");
+        case Rejected:
+            return tr("Rejected", "type (plural)");
+        case Unverified:
+        default:
+            return tr("Unverified", "type (plural)");
+        }
+    }
+}
+
+QString TSampleInfo::tagsToString(const QStringList &tags)
+{
+    return tags.join(", ");
+}
+
+QStringList TSampleInfo::tagsFromString(const QString &s)
+{
+    static QRegExp rx("\\,\\s*");
+    return s.split(rx, QString::SkipEmptyParts);
+}
 
 /*============================== Public constructors =======================*/
 
@@ -92,9 +167,54 @@ void TSampleInfo::setId(quint64 id)
     d_func()->id = id;
 }
 
+void TSampleInfo::setAuthorId(quint64 id)
+{
+    d_func()->authorId = id;
+}
+
+void TSampleInfo::setAuthorName(const QString &name)
+{
+    d_func()->authorName = name;
+}
+
 void TSampleInfo::setTitle(const QString &title)
 {
     d_func()->title = title;
+}
+
+void TSampleInfo::setType(Type t)
+{
+    d_func()->type = t;
+}
+
+void TSampleInfo::setTags(const QStringList &list)
+{
+    d_func()->tags = list;
+}
+
+void TSampleInfo::setComment(const QString &s)
+{
+    d_func()->comment = s;
+}
+
+void TSampleInfo::setAdminRemark(const QString &s)
+{
+    d_func()->remark = s;
+}
+
+void TSampleInfo::setRating(quint8 r)
+{
+    d_func()->rating = r;
+}
+
+void TSampleInfo::setCreationDateTime(const QDateTime &dt)
+{
+    d_func()->creationDT = dt.toUTC();
+}
+
+void TSampleInfo::setModificationDateTime(const QDateTime &dt)
+{
+    d_func()->modificationDT = dt.toUTC();
 }
 
 quint64 TSampleInfo::id() const
@@ -102,36 +222,171 @@ quint64 TSampleInfo::id() const
     return d_func()->id;
 }
 
+QString TSampleInfo::idString(int fixedLength) const
+{
+    return TSampleInfoPrivate::numberToString(d_func()->id, fixedLength);
+}
+
+quint64 TSampleInfo::authorId() const
+{
+    return d_func()->authorId;
+}
+
+QString TSampleInfo::authorIdString(int fixedLength) const
+{
+    return TSampleInfoPrivate::numberToString(d_func()->authorId, fixedLength);
+}
+
+QString TSampleInfo::authorName() const
+{
+    return d_func()->authorName;
+}
+
 QString TSampleInfo::title() const
 {
     return d_func()->title;
 }
 
+TSampleInfo::Type TSampleInfo::type() const
+{
+    return d_func()->type;
+}
+
+QString TSampleInfo::typeString() const
+{
+    return typeToString(d_func()->type);
+}
+
+QStringList TSampleInfo::tags() const
+{
+    return d_func()->tags;
+}
+
+QString TSampleInfo::tagsString() const
+{
+    return tagsToString(d_func()->tags);
+}
+
+QString TSampleInfo::comment() const
+{
+    return d_func()->comment;
+}
+
+QString TSampleInfo::adminRemark() const
+{
+    return d_func()->remark;
+}
+
+quint8 TSampleInfo::rating() const
+{
+    return d_func()->rating;
+}
+
+QString TSampleInfo::ratingString(const QString &format) const
+{
+    QString f = (format.count("%r") == 1 && !format.contains("%1")) ? format : "%r";
+    return f.replace("%r", "%1").arg(d_func()->rating);
+}
+
+QDateTime TSampleInfo::creationDateTime(Qt::TimeSpec spec) const
+{
+    return d_func()->creationDT.toTimeSpec(spec);
+}
+
+QDateTime TSampleInfo::modificationDateTime(Qt::TimeSpec spec) const
+{
+    return d_func()->modificationDT.toTimeSpec(spec);
+}
+
+bool TSampleInfo::isValid() const
+{
+    const B_D(TSampleInfo);
+    return d->id && d->authorId && !d->authorName.isEmpty() && !d->title.isEmpty()
+            && d->creationDT.isValid() && d->modificationDT.isValid();
+}
+
 /*============================== Public operators ==========================*/
 
-TSampleInfo &TSampleInfo::operator=(const TSampleInfo &other)
+TSampleInfo &TSampleInfo::operator =(const TSampleInfo &other)
 {
     B_D(TSampleInfo);
     const TSampleInfoPrivate *dd = other.d_func();
     d->id = dd->id;
+    d->authorId = dd->authorId;
+    d->authorName = dd->authorName;
     d->title = dd->title;
+    d->type = dd->type;
+    d->tags = dd->tags;
+    d->comment = dd->comment;
+    d->remark = dd->remark;
+    d->rating = dd->rating;
+    d->creationDT = dd->creationDT;
+    d->modificationDT = dd->modificationDT;
     return *this;
+}
+
+bool TSampleInfo::operator ==(const TSampleInfo &other) const
+{
+    const B_D(TSampleInfo);
+    const TSampleInfoPrivate *dd = other.d_func();
+    return d->id == dd->id;
+    /* && d->authorId == dd->authorId && d->authorName == dd->authorName && d->title == dd->title
+            && d->type == dd->type && d->tags == dd->tags && d->comment == dd->comment && d->remark == dd->remark
+            && d->rating == dd->rating && d->creationDT == dd->creationDT && d->modificationDT == dd->modificationDT*/
+}
+
+TSampleInfo::operator QVariant() const
+{
+    return QVariant::fromValue(*this);
 }
 
 /*============================== Public friend operators ===================*/
 
-QDataStream &operator<<(QDataStream &stream, const TSampleInfo &info)
+QDataStream &operator <<(QDataStream &stream, const TSampleInfo &info)
 {
     const TSampleInfoPrivate *d = info.d_func();
     stream << d->id;
+    stream << d->authorId;
+    stream << d->authorName;
     stream << d->title;
+    stream << (int) d->type;
+    stream << d->tags;
+    stream << d->comment;
+    stream << d->remark;
+    stream << d->rating;
+    stream << d->creationDT;
+    stream << d->modificationDT;
     return stream;
 }
 
-QDataStream &operator>>(QDataStream &stream, TSampleInfo &info)
+QDataStream &operator >>(QDataStream &stream, TSampleInfo &info)
 {
     TSampleInfoPrivate *d = info.d_func();
     stream >> d->id;
+    stream >> d->authorId;
+    stream >> d->authorName;
     stream >> d->title;
+    int t = TSampleInfo::Unverified;
+    stream >> t;
+    d->type = static_cast<TSampleInfo::Type>(t);
+    stream >> d->tags;
+    stream >> d->comment;
+    stream >> d->remark;
+    stream >> d->rating;
+    stream >> d->creationDT;
+    if (d->creationDT.timeSpec() != Qt::UTC)
+        d->creationDT = d->creationDT.toUTC();
+    stream >> d->modificationDT;
+    if (d->modificationDT.timeSpec() != Qt::UTC)
+        d->modificationDT = d->modificationDT.toUTC();
     return stream;
+}
+
+QDebug operator <<(QDebug dbg, const TSampleInfo &info)
+{
+    const TSampleInfoPrivate *d = info.d_func();
+    dbg.nospace() << "TSampleInfo(" << d->id << "," << d->authorId << "," << d->authorName << "," << d->title << ","
+                  << info.typeString() << "," << info.ratingString() << "," << d->creationDT << ","
+                  << d->modificationDT << ")";
+    return dbg.space();
 }
--- src/tsampleinfo.h
+++ src/tsampleinfo.h
@@ -4,7 +4,10 @@
 class TSampleInfoPrivate;
 
 class QDataStream;
-class QString;
+class QStringList;
+class QDateTime;
+class QVariant;
+class QDebug;
 
 #include "tglobal.h"
 
@@ -12,6 +15,8 @@ class QString;
 #include <BBase>
 
 #include <QMetaType>
+#include <QString>
+#include <QCoreApplication>
 
 /*============================================================================
 ================================ TSampleInfo =================================
@@ -20,6 +25,18 @@ class QString;
 class TSMP_EXPORT TSampleInfo : public BBase
 {
     B_DECLARE_PRIVATE(TSampleInfo)
+    Q_DECLARE_TR_FUNCTIONS(TSampleInfo)
+public:
+    enum Type
+    {
+        Unverified = 0,
+        Approved = 10,
+        Rejected = 100
+    };
+public:
+    static QString typeToString(Type t, bool singular = true);
+    static QString tagsToString(const QStringList &tags);
+    static QStringList tagsFromString(const QString &s);
 public:
     explicit TSampleInfo();
     TSampleInfo(const TSampleInfo &other);
@@ -28,14 +45,41 @@ protected:
     explicit TSampleInfo(TSampleInfoPrivate &d);
 public:
     void setId(quint64 id);
+    void setAuthorId(quint64 id);
+    void setAuthorName(const QString &name);
     void setTitle(const QString &title);
+    void setType(Type t);
+    void setTags(const QStringList &list);
+    void setComment(const QString &s);
+    void setAdminRemark(const QString &s);
+    void setRating(quint8 r);
+    void setCreationDateTime(const QDateTime &dt);
+    void setModificationDateTime(const QDateTime &dt);
     quint64 id() const;
+    QString idString(int fixedLength = -1) const;
+    quint64 authorId() const;
+    QString authorIdString(int fixedLength = -1) const;
+    QString authorName() const;
     QString title() const;
+    Type type() const;
+    QString typeString() const;
+    QStringList tags() const;
+    QString tagsString() const;
+    QString comment() const;
+    QString adminRemark() const;
+    quint8 rating() const;
+    QString ratingString(const QString &format = "%r") const;
+    QDateTime creationDateTime(Qt::TimeSpec spec = Qt::UTC) const;
+    QDateTime modificationDateTime(Qt::TimeSpec spec = Qt::UTC) const;
+    bool isValid() const;
 public:
-    TSampleInfo &operator=(const TSampleInfo &other);
+    TSampleInfo &operator =(const TSampleInfo &other);
+    bool operator ==(const TSampleInfo &other) const;
+    operator QVariant() const;
 public:
-    friend QDataStream &operator<<(QDataStream &stream, const TSampleInfo &info);
-    friend QDataStream &operator>>(QDataStream &stream, TSampleInfo &info);
+    friend QDataStream &operator <<(QDataStream &stream, const TSampleInfo &info);
+    friend QDataStream &operator >>(QDataStream &stream, TSampleInfo &info);
+    friend QDebug operator <<(QDebug dbg, const TSampleInfo &info);
 };
 
 Q_DECLARE_METATYPE(TSampleInfo)
